ef2cb9a14e6148da3753f007a8ce23bad2e6ec94
time4breakfast <anjaeschaefer@yahoo.de>
2023-11-21 09:53:24 +0100

    Update namesarchive.h

--- namesarchive.h
+++ namesarchive.h
@@ -1,14 +1,7 @@
 #ifndef NAMEARCHIVE_H
 #define NAMEARCHIVE_H
 
-// Maximale Namenslänge
-#define MAX_NAME_LEN 10
-
-// Maximale Anzahl an Namen
-#define MAX_NAMES 100
-
-// Fügt einen Namen hinzu. Im Fehlerfall (Archiv ist voll) soll 0, ansonsten 1 zurückgegeben werden.
-// Ist der angegebene Name länger als die zulässige Namenslänge, wird der Name abgeschnitten hinzugefügt. Rückgabe ist dann 1.
+// Fügt einen Namen hinzu. Im Fehlerfall (kein Speicher mehr) soll 0, ansonsten 1 zurückgegeben werden.
 int addName(const char *name);
 // Wie addName. Fügt Namen aber direkt sortiert hinzu. Voraussetzung ist ein bereits sortiertes Archiv.
 int addNameSorted(const char *name);
@@ -18,5 +11,7 @@ int removeName(const char *name);
 void sortNames();
 // Gibt die Namen zeilenweise aus.
 void printNames();
+// Leert das Archiv
+void clearArchive();
 
 #endif
